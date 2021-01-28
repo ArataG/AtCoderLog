3a7392d31cbf52d372d092179235486f0d2ba26f
Arata <mau8fg@gmail.com>
2021-01-28 09:48:21 +0900

    add abc189

---
+++ ABC/abc189/a.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define REP(i,n) for (int i = 0; i <(n); ++i)
+#define DREP(i,n) for (int i = (n-1); i >=0; --i)
+#define REP2(i,x,n) for (int i = x; i <(n); ++i)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+static const double PI = acos(-1);
+static const int INF = 1e9+7;
+//debug 
+#ifdef _DEBUG
+#define debug(var)  do{cout << #var << " :";view(var);}while(0)
+#else
+#define debug(...)
+#endif
+template<typename T> void view(T e){cout << e << endl;}
+template<typename T> void view(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+template<typename T> void view(const vector<vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
+template<typename T1, typename T2> void view(pair<T1,T2> e) { cout << "("<< e.first << ", " << e.second <<")"  << endl;}
+template<typename T1, typename T2> void view(const vector<pair<T1,T2>>& v) {for(const auto& e: v){view(e);}}
+// end debug
+
+
+int main(){
+  //#define int long long
+  string s;
+  cin >> s;
+  if(s[0] == s[1] && s[1] == s[2]) cout << "Won" << endl;
+  else cout << "Lost" << endl;
+  return 0;
+}
